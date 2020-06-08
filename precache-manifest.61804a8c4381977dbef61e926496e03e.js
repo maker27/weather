cc4bb8c0940fd225da2c0b851414d6e5f9eada30
maker27 <evgeniy@maker27.ru>
2020-06-08 14:26:47 +0300

    Updates

---
+++ precache-manifest.61804a8c4381977dbef61e926496e03e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df2c7336f15d3dedeb23e52ca12a230b",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "066c155cc6b9f60f7e38",
+    "url": "/weather/static/css/2.f296aa83.chunk.css"
+  },
+  {
+    "revision": "0b911356013c8572a073",
+    "url": "/weather/static/css/main.7f9b462c.chunk.css"
+  },
+  {
+    "revision": "066c155cc6b9f60f7e38",
+    "url": "/weather/static/js/2.8af1ecad.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/weather/static/js/2.8af1ecad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b911356013c8572a073",
+    "url": "/weather/static/js/main.90279114.chunk.js"
+  },
+  {
+    "revision": "b83e05f3b1e38480a3ef",
+    "url": "/weather/static/js/runtime-main.d16f45af.js"
+  }
+]);
